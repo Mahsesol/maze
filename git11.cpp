804a35876bb0f773247fa8b87ede7a9c52b1d1ef
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-01-20 21:20:42 +0330

    Add files via upload

---
+++ git11.cpp
@@ -0,0 +1,12 @@
+void updateuserinfo(userinfo user, int gameresult, int playingtime) {
+    user.playcount++;
+    user.playingtime += playingtime;
+    if (gameresult == 1) {
+        user.wincount++;
+        user.lastwin = get_localtime();
+        user.winplayingtime += playingtime;
+    }
+    string name = user.name + ".txt";
+    ofstream userfile(name);
+    userfile << user.name << "\n" << user.playcount << "\n" << user.wincount << "\n" << user.lastwin << "\n" << user.playingtime << "\n" << user.winplayingtime << "\n";
+}
