4b48d7a4fb59c9867f5402535bfacd617ea1e0e6
Mahsa Soleimani <154425249+Mahsesol@users.noreply.github.com>
2024-01-29 00:51:29 +0330

    Update git19.cpp

--- git19.cpp
+++ git19.cpp
@@ -1,8 +1,11 @@
+
+
+//function that print the leaderboard
 void showtopplayers() {
+    cout << "\u001b[36;1m******** LEADER BOARD ********\u001b[0m" << endl << endl;
     ifstream topplayerfile("topplayers.txt");
-    string timeunit[3];
-    double time[3];
     topplayer tops[3];
+    struct time playtime[3];
     for (int i = 0; i < 3; i++) {
         string wincount, winplayingtime;
         getline(topplayerfile, tops[i].name);
@@ -10,17 +13,18 @@ void showtopplayers() {
         getline(topplayerfile, winplayingtime);
         tops[i].wincount = stoi(wincount);
         tops[i].playingtime = stoi(winplayingtime);
-        if (tops[i].playingtime >= 60) {
-            time[i] = tops[i].playingtime / 60;
-            timeunit[i] = "minutes";
-        }
-        else {
-            time[i] = tops[i].playingtime;
-            timeunit[i] = "seconds";
-        }
+        playtime[i].minutes = tops[i].playingtime / 60;
+        playtime[i].seconds = tops[i].playingtime % 60;
     }
 
     for (int i = 0; i < 3; i++) {
-        cout<< endl << i + 1 << "." << tops[i].name << endl << "score: " << tops[i].wincount << endl << "playing time: " << time[i] << " " << timeunit[i] << endl;
+        if (1 % 2 == 0) {
+            cout << endl << i + 1 << "\u001b[33;1m. Name:" << "\u001b[0m" << tops[i].name << endl << " \u001b[33;1mScore: " << "\u001b[0m" << tops[i].wincount << endl << "\u001b[33;1mPlaying time: " << "\u001b[0m" << playtime[i].minutes << " minutes and  " << playtime[i].seconds << " seconds" << endl << endl;
+        }
+        else {
+            cout << endl << i + 1 << "\u001b[35;1m. Name:" << "\u001b[0m" << tops[i].name << endl << " \u001b[35;1mScore: " << "\u001b[0m" << tops[i].wincount << endl << "\u001b[35;1mPlaying time: " << "\u001b[0m" << playtime[i].minutes << " minutes and  " << playtime[i].seconds << " seconds" << endl << endl;
+        }
     }
+
 }
+
