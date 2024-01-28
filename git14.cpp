6acdc57839248aa3349830dc9daf0b652cf63689
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-01-29 01:09:31 +0330

    Update git14.cpp

--- git14.cpp
+++ git14.cpp
@@ -1,8 +1,13 @@
+
+//struct to save the top players information
 struct topplayer {
     string name;
     int wincount;
     time_t playingtime;
 };
+
+
+//function to update top players after every game and save to a file
 void updatetopplayers(userinfo user) {
     string wincount, winplayingtime;
     topplayer thisuser;
@@ -11,20 +16,22 @@ void updatetopplayers(userinfo user) {
     thisuser.playingtime = user.winplayingtime;
     ifstream topplayerfile("topplayers.txt");
     topplayer tops[4];
+    topplayer tmptops[3];
     int check = 0;
-  
+
     for (int i = 0; i < 3; i++) {
         getline(topplayerfile, tops[i].name);
         getline(topplayerfile, wincount);
         getline(topplayerfile, winplayingtime);
         tops[i].wincount = stoi(wincount);
         tops[i].playingtime = stoi(winplayingtime);
+        tmptops[i] = tops[i];
     }
     for (int i = 0; i < 3; i++) {
         if (thisuser.name == tops[i].name) {
             check++;
             break;
-                
+
         }
     }
 
@@ -71,12 +78,21 @@ void updatetopplayers(userinfo user) {
                     }
                 }
             }
-            
+
         }
     }
     ofstream newtops("topplayers.txt");
     for (int i = 0; i < 3; i++) {
         newtops << tops[i].name << "\n" << tops[i].wincount << "\n" << tops[i].playingtime << "\n";
     }
-
+    // int check1 = 0;
+    // for (int i = 0; i < 3; i++) {
+      //   if (tops[i].name != tmptops[i].name) {
+      //       check1++;
+           //  break;
+      //   }
+    // }
+    // check1 = 0;
+    // return check1;
 }
+
