1ac7fe30cbd5c0c07259f2ef459a0eb5bb3be73d
Mahsa Soleimani <154425249+Mahsesol@users.noreply.github.com>
2024-01-29 00:03:08 +0330

    Update git5.cpp

--- git5.cpp
+++ git5.cpp
@@ -1,11 +1,14 @@
-int maxelement(vector<vector<int>> matrix){
+
+int maxelement(vector<vector<int>> matrix) {
     int max = std::numeric_limits<int>::min();
 
     for_each(matrix.begin(), matrix.end(),
         [&max](const auto& v)
         {
-           max = std::max(*max_element(v.begin(), v.end()), max);
+            max = std::max(*max_element(v.begin(), v.end()), max);
         }
     );
     return max;
 }
+
+
