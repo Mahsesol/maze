01b3bc6edf75a3631cdca6326e1cee6388b50159
Mahsa Soleimani <154425249+Mahsesol@users.noreply.github.com>
2024-01-29 00:49:40 +0330

    Update git18.cpp

--- git18.cpp
+++ git18.cpp
@@ -1,16 +1,18 @@
+
+
+//function that shows game history
 void showhistory(vector<history> vec) {
-    cout << endl << "*GAME HISTORY*" << endl;
-    string timeunit[10];
-    double time[10];
+    cout << endl << "\u001b[36;1m********* GAME HISTORY ********\u001b[0m" << endl << endl;
+    struct time playtime[10];
     for (int i = 9; i >= 0; i--) {
-        if (vec[i].playingtime >= 60) {
-            time[i] = vec[i].playingtime / 60;
-            timeunit[i] = "minutes";
+        playtime[i].minutes = vec[i].playingtime / 60;
+        playtime[i].seconds = vec[i].playingtime % 60;
+        if (i % 2 == 0) {
+            cout << 10 - i << "\u001b[35;1m.Player name: " << "\u001b[0m " << vec[i].playername << endl << "\u001b[35;1mMap name: " << "\u001b[0m " << vec[i].mapname << endl << "\u001b[35;1mResult: " << "\u001b[0m " << vec[i].result << endl << "\u001b[35;1mDate: " << "\u001b[0m " << vec[i].date << endl << "\u001b[35;1mPlaying time: " << "\u001b[0m " << playtime[i].minutes << " minutes and  " << playtime[i].seconds << " seconds" << endl << endl;
         }
         else {
-            time[i] = vec[i].playingtime;
-            timeunit[i] = "seconds";
+            cout << 10 - i << "\u001b[33;1m.Player name: " << "\u001b[0m " << vec[i].playername << endl << "\u001b[33;1mMap name: " << "\u001b[0m " << vec[i].mapname << endl << "\u001b[33;1mResult: " << "\u001b[0m " << vec[i].result << endl << "\u001b[33;1mDate: " << "\u001b[0m " << vec[i].date << endl << "\u001b[33;1mPlaying time: " << "\u001b[0m " << playtime[i].minutes << " minutes and  " << playtime[i].seconds << " seconds" << endl << endl;
         }
-        cout << 10 - i << ".playername: " << vec[i].playername << endl << "mapname: " << vec[i].mapname << endl << "result: " << vec[i].result << endl << "date: " << vec[i].date << endl << "playingtime: " << time[i] << " " << timeunit[i] << endl << endl;
     }
 }
+
