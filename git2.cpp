86e8538e30250fce1e94590a20b4b12444229be5
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-01-29 00:54:15 +0330

    Update git2.cpp

--- git2.cpp
+++ git2.cpp
@@ -1,13 +1,19 @@
+
+
+//function to print mazes
 void printmaze(const vector<vector<int>>& array, int min, int max) {
     const int numRows = array.size();
     const int numCols = (numRows > 0) ? array[0].size() : 0;
 
-   
+    // ANSI escape codes for text color
     const char* greenText = "\033[1;32m";
     const char* redText = "\033[1;31m";
     const char* yellowText = "\033[1;33m";
     const char* resetText = "\033[0m";
 
+
+
+    // Print the top border
     cout << "*";
     for (int j = 0; j < numCols; ++j) {
         cout << "-----*";
@@ -15,6 +21,7 @@ void printmaze(const vector<vector<int>>& array, int min, int max) {
     cout << endl;
 
 
+    // Print the array with borders
     for (int i = 0; i < numRows; ++i) {
         cout << "|";
         for (int j = 0; j < numCols; ++j) {
@@ -24,25 +31,25 @@ void printmaze(const vector<vector<int>>& array, int min, int max) {
             }
 
             else if (i == numRows - 1 && j == numCols - 1) {
-                
+                // Bottom right corner: print in red
                 cout << redText << setw(5) << currentValue << resetText << "|";
             }
             else {
-                
+                // Check if the value is less than -5
                 if (currentValue < min && currentValue != 0) {
 
 
                     cout << greenText << setw(5) << currentValue + abs(max) * 2 + abs(min) * 2 + 1 << resetText << "|";
                 }
                 else {
-                 
+                    // Otherwise, print normally
                     cout << setw(5) << currentValue << "|";
                 }
             }
         }
         cout << endl;
 
-        
+        // Print the mid border
         cout << "*";
         for (int j = 0; j < numCols; ++j) {
             cout << "-----*";
@@ -50,3 +57,4 @@ void printmaze(const vector<vector<int>>& array, int min, int max) {
         cout << endl;
     };
 }
+
