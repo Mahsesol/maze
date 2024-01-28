751c87a161b6efe380347995d5bb99dceece7179
Mahsa Soleimani <154425249+Mahsesol@users.noreply.github.com>
2024-01-28 23:53:46 +0330

    Update git00.cpp

--- git00.cpp
+++ git00.cpp
@@ -3,20 +3,18 @@
 #include <iomanip>
 #include <algorithm>
 #include <random>
-#include<cmath>
+#include <cmath>
 #include <chrono>
 #include <thread>
 #include <limits>
 #include <fstream>
 #include <ctime>
 #include <chrono>
-#include <cstdio>
+#include <cstdlib>
+//#include <curses.h>
+#include <conio.h>
+
 
 
 using namespace std;
 using namespace std::chrono;
-
-
-void newmap(vector<vector<int>> vec, int row, int col, int steps,string mapname);
-
-const int SPECIAL_VALUE = std::numeric_limits<int>::max();
