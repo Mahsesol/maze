76fb8f43b9cb95aa07e94cb21984e9332668be46
Mahsa Soleimani <154425249+Mahsesol@users.noreply.github.com>
2024-01-29 00:00:52 +0330

    Update git4.cpp

--- git4.cpp
+++ git4.cpp
@@ -1,11 +1,16 @@
-int minelement(vector<vector<int>> matrix){
-    int min = std::numeric_limits<int>::max();
 
-    for_each(matrix.begin(), matrix.end(),
-        [&min](const auto& v)
-        {
-           min = std::min(*min_element(v.begin(), v.end()), min);
+//function that add any new map made with program in the maps list in playground
+void newmap(vector<vector<int>> vec, int row, int col, int steps, string mapname) {
+    mapname = mapname + ".txt";
+    ofstream file(mapname);
+    file << row << " " << col << '\n' << steps << '\n';
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            file << vec[i][j] << " ";
         }
-    );
-    return min;
+        file << '\n';
+    }
+    ofstream mapsfile("maps.txt", ios::app);
+    mapsfile << "\n" << mapname;
 }
+
