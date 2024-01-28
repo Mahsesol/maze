56aba902300be5ecf3a6e97167454a8d3e86afa1
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-01-29 01:11:40 +0330

    Update git17.cpp

--- git17.cpp
+++ git17.cpp
@@ -1,6 +1,8 @@
+
+// function to solve any maze
 bool mazesolution(vector<vector<int>>& maze, int x, int y, int destX, int destY, int steps, int min, int max, int sum, vector<vector<int>>& maze1) {
 
-
+    // Base cases
     if (!isValid(x, y, maze.size(), maze[0].size())) {
         return false;
     }
@@ -21,6 +23,7 @@ bool mazesolution(vector<vector<int>>& maze, int x, int y, int destX, int destY,
         return true;
     }
 
+    // Recursive backtracking
     sum += maze[x][y];
     int change = 2 * (abs(max) + abs(min)) + 1;
     maze[x][y] -= change;
@@ -29,3 +32,10 @@ bool mazesolution(vector<vector<int>>& maze, int x, int y, int destX, int destY,
         || mazesolution(maze, x, y + 1, destX, destY, steps - 1, min, max, sum, maze1) || mazesolution(maze, x, y - 1, destX, destY, steps - 1, min, max, sum, maze1)) {
         return true;
     }
+
+
+    // If no valid path found, backtrack
+    maze[x][y] += change;
+    sum -= maze[x][y];
+    return false;
+}
